70fcbb827b9cdac8d753f9fa4e8beec4a83ce80a
javivi09dev <javierotoalonso@gmail.com>
2024-06-25 12:42:51 +0200

    ...

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'd1538b43-1083-43ac-89d5-c88cb0049ada'
 // SEE NOTE ABOVE.
 
 
