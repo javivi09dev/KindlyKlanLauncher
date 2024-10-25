066b2d05dc4c8a997362e0701aff757dcec8d288
javivi09dev <javierotoalonso@gmail.com>
2024-10-25 16:20:54 +0200

    Update cambio de ip

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://23.159.176.122:26500/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://23.146.144.188:26500/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
