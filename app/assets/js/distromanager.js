9cc870504866c5ba4db436573a2c0c3e31b0a5d7
javivi09dev <javierotoalonso@gmail.com>
2024-11-10 20:57:38 +0100

    Actualización de ip: files.kindlyklan.com

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -1,10 +1,9 @@
 const { DistributionAPI } = require('helios-core/common')
-
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://23.146.144.188:26500/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://files.kindlyklan.com:26500/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
